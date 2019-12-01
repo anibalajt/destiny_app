48e3535947b2d9670b17986b9aaf95d450a1e3fb
anibalajt <anibalajt@gmail.com>
2019-12-01 21:40:04 +0800

    api key

---
+++ app/utils/constants.js
@@ -0,0 +1,6 @@
+export default {
+  API_Key: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXCXXC",
+  OAuth_Authorization_URL: "https://www.bungie.net/en/OAuth/Authorize",
+  OAuth_client_id: "XXXXXXX",
+  OAuth_client_secret: "XXXXXXXXXXX"
+}
