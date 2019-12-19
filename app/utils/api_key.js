b412564a86059cb37c0338305911d266f45901a6
anibalajt <anibalajt@gmail.com>
2019-12-19 15:02:26 +0800

    gitignore

--- app/utils/api_key.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  API_Key: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXCXXC",
-  OAuth_Authorization_URL: "https://www.bungie.net/en/OAuth/Authorize",
-  OAuth_client_id: "XXXXXXX",
-  OAuth_client_secret: "XXXXXxXXXXXX0"
-}
