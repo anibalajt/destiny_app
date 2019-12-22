0a3686768712a200a57c1deb2674e59dc8b2aea7
anibalajt <anibalajt@gmail.com>
2019-12-22 23:55:25 +0800

    update func get languaje

--- app/utils/lenguaje.js
+++ app/utils/lenguaje.js
@@ -1,11 +1,17 @@
 import { NativeModules, Platform } from "react-native";
-
 export default () => {
 	let lenguaje = "en";
 	if (Platform.OS === "android") {
 		lenguaje = NativeModules.I18nManager.localeIdentifier.split("_")[0];
 	} else {
-		lenguaje = NativeModules.SettingsManager.settings.AppleLocale.split("_")[0];
+		let locale = NativeModules.SettingsManager.settings.AppleLocale // "fr_FR"
+		if (locale === undefined) {
+			// iOS 13 workaround, take first of AppleLanguages array  ["en", "en-NZ"]
+			locale = NativeModules.SettingsManager.settings.AppleLanguages[0]
+			if (locale == undefined) {
+				lenguaje = "en" // default language
+			}
+		}
 	}
 	lenguaje !== "en" && lenguaje !== "es" && (lenguaje = "en");
 	return lenguaje;
