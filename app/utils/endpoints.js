dfed09269fcac12ff122f6258e9b60378871bd83
anibalajt <anibalajt@gmail.com>
2019-12-29 19:33:38 +0800

    endpoint get manifest

--- app/utils/endpoints.js
+++ app/utils/endpoints.js
@@ -1,5 +1,5 @@
 module.exports = {
   assets: "https://www.bungie.net",
-  userThemes: (profileThemeName) => `https://www.bungie.net/img/UserThemes/${profileThemeName}/mobiletheme.jpg`
-
+  userThemes: (profileThemeName) => `https://www.bungie.net/img/UserThemes/${profileThemeName}/mobiletheme.jpg`,
+  getManifest: "https://www.bungie.net/Platform/Destiny2/Manifest/"
 }
